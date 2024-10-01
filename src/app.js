9f5cac91d9737d4b7b97e72fafdc0e88def89f36
Imran <imranshah3055@gmail.com>
2024-10-01 12:12:57 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,18 +2,23 @@ const express = require('express');
 
 const app = express();
 
-
-app.use("/", (req, res) => {
-    res.send('Assalamalikum Imran')
+app.get('/user', (req, res) => {
+    res.send("Fetched Data Successfully");
 })
 
-app.use("/hello", (req, res) => {
-    res.send('Hello Hello Hello h');
+app.post('/user', (req, res) => {
+    res.send({firstname : "Imran", lastname : "Shah"})
+})
+
+app.delete('/user', (req, res) => {
+    res.send("Deleted user successfully");
 })
 
 app.use("/test", (req, res)=> {
     res.send("This is test route")
 })
+
+
 app.listen(7777, () => {
     console.log('Server is running successfully on the port 7777...');
 });
