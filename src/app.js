fc35ab8e68caecab7755b3eb8945d78d6553af6b
Imran <imranshah3055@gmail.com>
2024-10-03 16:38:37 +0530

    delete-update API's

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/signup', async (req, res) => {
+app.post("/signup", async (req, res) => {
   // Creating a new instance of the User model
   const user = new User(req.body);
 
@@ -15,17 +15,17 @@ app.post('/signup', async (req, res) => {
   } catch (error) {
     res.status(400).send("Error saving user!!", err.message);
   }
-})
+});
 
 // Get user by emailId
-app.get('/user', async (req, res) => {
+app.get("/user", async (req, res) => {
   const Useremail = req.body.emailId;
   try {
-    const users = await User.findOne({emailId : Useremail});
-    if(users.length === 0){
+    const users = await User.findOne({ emailId: Useremail });
+    if (users.length === 0) {
       res.send(404).send("User not found!");
-    }else{
-      res.send(users); 
+    } else {
+      res.send(users);
     }
   } catch (error) {
     res.status(400).send("Somthing went wrong!");
@@ -33,14 +33,44 @@ app.get('/user', async (req, res) => {
 });
 
 // Feed API - Get /feed - get all the users from database
-app.get('/feed', async (req, res) => {
+app.get("/feed", async (req, res) => {
   try {
     const users = await User.find({});
-   res.send(users); 
+    res.send(users);
   } catch (err) {
     res.send(400).send("Somthing went wrong!");
   }
-})
+});
+
+// Delete a user from database
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    if (!user) {
+      res.status(404).send("User not found!");
+    } else {
+      res.send("User deleted successfully...");
+    }
+  } catch (error) {
+    res.status(400).send("Somthing went wrong!");
+  }
+});
+
+// update a user from database
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+    });
+    console.log(user);
+    res.send("User Updated Successfully...");
+  } catch (error) {
+    res.status(400).send("Somthing went wrong!");
+  }
+});
 
 connectDB()
   .then(() => {
