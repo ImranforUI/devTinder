354fc065145494de4793d072c63fd32418da1c4c
Imran <imranshah3055@gmail.com>
2024-10-06 16:12:05 +0530

    Login API

--- src/app.js
+++ src/app.js
@@ -1,19 +1,54 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/user");
+const { validationSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
 const app = express();
 
 app.use(express.json());
 
+// Creating a User
 app.post("/signup", async (req, res) => {
-  // Creating a new instance of the User model
-  const user = new User(req.body);
-
   try {
+    // Validation of User
+    validationSignUpData(req);
+
+    const { firstName, lastName, emailId, password } = req.body;
+    // Encrypting Password
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    // Creating a new instance of the User model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+
     await user.save();
     res.send("User Added Successfully...");
   } catch (error) {
-    res.status(400).send("Error saving user!!", err.message);
+    res.status(400).send("Error saving user!" + error.message);
+  }
+});
+
+// Login Api
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid credentials");
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (isPasswordValid) {
+      res.send("Login successfully...");
+    } else {
+      throw new Error("Invalid credentials");
+    }
+  } catch (error) {
+    res.status(400).send("Error saving user!" + error.message);
   }
 });
 
@@ -62,13 +97,18 @@ app.patch("/user/:userId", async (req, res) => {
   const userId = req.params.userId;
   const data = req.body;
   try {
-    const ALLOWED_UPDATES = ['photoUrl', "about", "gender", "age", "skills"];
-    const isUpdateAllowed = Object.keys(data).every(k => ALLOWED_UPDATES.includes(k));
-    if(!isUpdateAllowed){
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+    if (!isUpdateAllowed) {
       throw new Error("Update not allowed");
     }
-    if(data?.skills.length > 10){
+    if (data?.skills.length > 10) {
       throw new Error("Skills cannot be more than 10");
+    }
+    if (data?.age < 18) {
+      throw new Error("Age must be 18 or above");
     }
     const user = await User.findByIdAndUpdate({ _id: userId }, data, {
       returnDocument: "after",
--- src/models/user.js
+++ src/models/user.js
@@ -1,29 +1,59 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = mongoose.Schema({
-    firstName : { type : String, maxLength : 50, minLenght : 4},
-    lastName : { type : String},
-    emailId : { type : String, unique : true, lowercase : true, required : true, time : true},
-    password : { type : String, required : true},
-    age : { type : Number, min : 18},
-    gender : { type : String,
-        validate(value){
-            if(!['male', 'female', 'others'].includes(value)){
-                throw new Error("Gender data is not valid");
-            }
-        },
+const userSchema = mongoose.Schema(
+  {
+    firstName: { type: String, maxLength: 50, minLenght: 4 },
+    lastName: { type: String },
+    emailId: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      required: true,
+      time: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address!");
+        }
+      },
     },
-    photoUrl : {
-        type : String,
-        default : "https://tse3.mm.bing.net/th?id=OIP.w0TcjC4y9CxTrY3sitYa_AAAAA&pid=Api&P=0&h=180",
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter Strong Password!");
+        }
+      },
     },
-    about : {
-        type : String,
-        default : "This is a default about of the user",
+    age: { type: Number, min: 18 },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
     },
-    skills : {
-       type : [String]
-    }
-},{ timestamps : true});
+    photoUrl: {
+      type: String,
+      default:
+        "https://tse3.mm.bing.net/th?id=OIP.w0TcjC4y9CxTrY3sitYa_AAAAA&pid=Api&P=0&h=180",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Enter valid photo URL!");
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is a default about of the user",
+    },
+    skills: {
+      type: [String],
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
