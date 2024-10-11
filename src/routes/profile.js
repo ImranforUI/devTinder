c9cb578512b6e5c0efdccbc5d606f25a25ad1e7d
Imran <imranshah3055@gmail.com>
2024-10-11 15:37:29 +0530

    logout + Eidt profile API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
+const { validateEditProfileData } = require('../utils/validation')
 
 // Get Profile
 profileRouter.get("/profile", userAuth, async (req, res) => {
-    console.log("Start...")
   try {
     const user = req.user;
 
@@ -13,5 +13,26 @@ profileRouter.get("/profile", userAuth, async (req, res) => {
     res.status(400).send("Error saving user!" + error.message);
   }
 });
+
+// Profile Edit
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if(!validateEditProfileData(req)){
+      throw new Error('Invalid Edit Request!!')
+    }
+    const loggedInUser = req.user;
+
+    Object.keys(req.body).forEach(key => (loggedInUser[key] = req.body[key]));
+
+    await loggedInUser.save();
+
+    res.json({ message : `${loggedInUser.firstName}, Your Profile Updated Sucessfully`,
+      data : loggedInUser,
+    });
+
+  } catch (error) {
+    res.status(400).send("ERROR : " + error.message);
+  }
+})
 
 module.exports = profileRouter;
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,18 +1,38 @@
-const validator = require('validator');
+const validator = require("validator");
 
 const validationSignUpData = (req) => {
-    const {firstName, lastName, emailId, password} = req.body;
-    if(!firstName || !lastName){
-        throw new Error('Name is not valid!');
-    }
-    if(!validator.isEmail(emailId)){
-        throw new Error("Email is not valid!");
-    }
-    if(!validator.isStrongPassword(password)){
-        throw new Error('Please enter strong password!');
-    }
-}
+  const { firstName, lastName, emailId, password } = req.body;
+  if (!firstName || !lastName) {
+    throw new Error("Name is not valid!");
+  }
+  if (!validator.isEmail(emailId)) {
+    throw new Error("Email is not valid!");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw new Error("Please enter strong password!");
+  }
+};
+
+const validateEditProfileData = (req) => {
+  const allowedEditFields = [
+    "firstName",
+    "lastName",
+    "emailId",
+    "photoUrl",
+    "gender",
+    "age",
+    "about",
+    "skills",
+  ];
+
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowedEditFields.includes(field)
+  );
+
+  return isEditAllowed;
+};
 
 module.exports = {
-    validationSignUpData
-}
+  validationSignUpData,
+  validateEditProfileData
+};
